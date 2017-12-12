a5171027d427b09c414c92e9da41aabfed51bc99
nesreenM <nesreen.mouti@gmail.com>
2017-12-12 17:52:56 +0200

    Initial Commit

---
+++ SearchTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SearchTask
+//
+//  Created by Nesreen Mamdouh on 12/12/17.
+//  Copyright © 2017 dlc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
